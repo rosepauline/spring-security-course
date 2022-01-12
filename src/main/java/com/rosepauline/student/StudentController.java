34f62cfb958b44c4b8e892bcc2714dce95503cd3
Admin <mukesh.kumar@jesperapps.com>
2022-01-12 17:36:16 +0530

    first push till user roles and authorities

---
+++ src/main/java/com/rosepauline/student/StudentController.java
@@ -0,0 +1,28 @@
+package com.rosepauline.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private  static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1,"Rose"),
+            new Student(2, "Pauline"),
+            new Student(3, "Ramya")
+    );
+
+    @GetMapping(path = "{studentId}")
+    public  Student getStudent(@PathVariable("studentId") Integer studentId){
+    return STUDENTS.stream()
+            .filter(student -> studentId.equals(student.getStudentId()))
+            .findFirst()
+            .orElseThrow(() -> new IllegalStateException("student "+ studentId + "does not exists"));
+    }
+}
---
+++ src/main/java/com/rosepauline/student/StudentManagementController.java
@@ -0,0 +1,42 @@
+package com.rosepauline.student;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("management/api/v1/students")
+public class StudentManagementController {
+
+    private  static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1,"Rose"),
+            new Student(2, "Pauline"),
+            new Student(3, "Ramya")
+    );
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        System.out.println("listStudents");
+        return  STUDENTS;
+    }
+
+    @PostMapping
+    public  void registerNewStudent(@RequestBody Student student){
+        System.out.println("postStudent");
+        System.out.println(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void  deleteStudent(@PathVariable("studentId") Integer studentId){
+        System.out.println("deleteStudent");
+        System.out.println(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Integer studentId, Student student){
+        System.out.println("putStudent");
+        System.out.println(String.format("%s %s", studentId, student));
+    }
+}
+
