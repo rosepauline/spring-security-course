1254454d9d68c8e2f5ae1ea78edf02eacde7d6d3
Admin <mukesh.kumar@jesperapps.com>
2022-01-12 17:58:06 +0530

    bootstrap-build an API

---
+++ src/main/java/com/rosepauline/SpringSecurityCourseAmigoscodeApplication.java
@@ -0,0 +1,13 @@
+package com.rosepauline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityCourseAmigoscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityCourseAmigoscodeApplication.class, args);
+	}
+
+}
