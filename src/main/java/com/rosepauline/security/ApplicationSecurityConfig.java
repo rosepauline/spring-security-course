6c40273565e9adf6264ef68ec97f445986275715
Admin <mukesh.kumar@jesperapps.com>
2022-01-17 16:22:23 +0530

    cross site request forgery and form based authentication

--- src/main/java/com/rosepauline/security/ApplicationSecurityConfig.java
+++ src/main/java/com/rosepauline/security/ApplicationSecurityConfig.java
@@ -13,6 +13,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.rosepauline.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.rosepauline.security.ApplicationUserRole.*;
@@ -33,23 +37,42 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                .and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/","index","/css/*","/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
-//                we have used @PreAuthorize in method and @EnableGlobalMethodSecurity(prePostEnabled = true) in ApplicationSecurityConfig class
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .defaultSuccessUrl("/courses", true)
+                    .passwordParameter("password1")
+                    .usernameParameter("username")
+                .and()
+                .rememberMe()//defaults to 2weeks
+                    .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
+                    .key("somethingverysecured")
+                    .rememberMeParameter("remember-me")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID", "remember-me")
+                    .logoutSuccessUrl("/login");
+
+    }
+
+    //                we have used @PreAuthorize in method and @EnableGlobalMethodSecurity(prePostEnabled = true) in ApplicationSecurityConfig class
 //                .antMatchers(HttpMethod.DELETE,"management/api/**").hasAuthority(COURSE_WRITE.getPermission())
 //                .antMatchers(HttpMethod.POST,"management/api/**").hasAuthority(COURSE_WRITE.getPermission())
 //                .antMatchers(HttpMethod.PUT,"management/api/**").hasAuthority(COURSE_WRITE.getPermission())
 //                .antMatchers(HttpMethod.GET, "management/api/**").hasAnyRole(ADMIN.name(),ADMINTRAINEE.name())
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
-
-
-    }
-
     @Override
     @Bean
     protected UserDetailsService userDetailsService() {
